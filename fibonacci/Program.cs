d0b1ab3fbcc087d398ae32e61e0ff736a28254eb
SkyTitan <gergo.barnabas.borka@umszki.hu>
2025-10-07 11:13:59 +0200

    hellobello

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int egyik = 0;
+int masik = 1;
+
+for (int i = 0; i < 10; i++);
+{
+    int következő = egyik + masik;
+    egyik = masik;
+    masik = következő;
+}
+
+Console.WriteLine(egyik);
+Console.WriteLine(masik);
