b54fa7b7962f09a7fdfe21b203637616d94d437b
SkyTitan <gergo.barnabas.borka@umszki.hu>
2025-10-07 10:15:24 +0200

    almasalma

--- xD/Program.cs
+++ xD/Program.cs
@@ -1,10 +1,20 @@
-﻿int vizsga = 123456789;
+﻿using System.Diagnostics.Contracts;
+
+int vizsga = 123456789;
 bool isPrime = true;
 
-for (int i = 2; i < vizsga; i++)
+for (int i = 2; i < vizsga && isPrime; i++)
 {
     if (vizsga % i == 0)
     {
-        isPrime = false;    
+        isPrime = false;
+
+        Console.WriteLine(i);
     }
 }
+
+if (isPrime)
+{
+    Console.WriteLine("Ige,n ez prím volt");
+}
+else
